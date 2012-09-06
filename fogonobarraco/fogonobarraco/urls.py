a1ad802c0f4e09202137c54bfaa7c10b3c3b8d17
Pedro Moraes <pedro@asa.tc>
2012-09-06 17:30:05 -0300

    fix url

--- fogonobarraco/fogonobarraco/urls.py
+++ fogonobarraco/fogonobarraco/urls.py
@@ -15,6 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'occurrences.views.home', name='home'),  
-    (r'^occurrences.json$', 'occurrences.views.get'),
+    url(r'^occurrences.json$', 'occurrences.views.get')
    
 )
