89ced515af6e62b67cf1c5e290cd11d642116bf2
Arthur Lima <arthur@jurema.la>
2012-09-23 21:42:39 -0300

    Para manter as configurações do ambiente local, crie arquivo settings_local.py

--- fogonobarraco/fogonobarraco/settings.py
+++ fogonobarraco/fogonobarraco/settings.py
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/a/Work/jurema/fogo-no-barraco/fogonobarraco/fogonob.sqlite3',                      # Or path to database file if using sqlite3.
+        'NAME': 'fogonob.sqlite3',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -157,3 +157,9 @@ LOGGING = {
         },
     }
 }
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
