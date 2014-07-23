21bb372232b1f3605763fab2150959bfef6a02d8
Pedro Moraes <pedromoraes@gmail.com>
2014-07-23 17:59:13 -0300

    jsop

--- fogonobarraco/occurrences/views.py
+++ fogonobarraco/occurrences/views.py
@@ -7,6 +7,28 @@ from django.http import HttpResponse
 from django.db.models import Count
 import simplejson
 
+def json_response(func):
+    """
+    A decorator thats takes a view response and turns it
+    into json. If a callback is added through GET or POST
+    the response is JSONP.
+    """
+
+    def decorator(request, *args, **kwargs):
+        objects = func(request, *args, **kwargs)
+        if isinstance(objects, HttpResponse):
+            return objects
+        try:
+            data = simplejson.dumps(objects)
+            if 'callback' in request.REQUEST:
+                # a jsonp response!
+                data = '%s(%s);' % (request.REQUEST['callback'], data)
+                return HttpResponse(data, "text/javascript")
+        except:
+            data = simplejson.dumps(str(objects))
+        return HttpResponse(data, "application/json")
+    return decorator
+
 def home(request):  
 	c = Context({ 'years': settings.RESEARCH_YEARS })  
 	return render_to_response("home.html", c, context_instance=RequestContext(request))
@@ -19,9 +41,10 @@ def get(request):
 		all.append(dict([(attr, getattr(occ, attr)) for attr in fields]))
 	return HttpResponse(simplejson.dumps({"success": True, "occurrences": all}),'application/json')
 
+@json_response
 def per_year(request):
 	qset = Occurrence.objects.filter(status='P').values('year').order_by().annotate(Count('year'))
 	years = []
 	for item in qset:
 		years.append({'year': item['year'], 'count': item['year__count']})
-	return HttpResponse(simplejson.dumps({"success": True, "years": years}),'application/json')
+	return {"success": True, "years": years}
